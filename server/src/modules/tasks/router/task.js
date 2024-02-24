2e8986f8f6718b260daba736b9da9b780c738555
abdul hannan <abdul.hannan@plaxonic.com>
2024-02-24 14:27:42 +0530

    task api done

--- server/src/modules/tasks/router/task.js
+++ server/src/modules/tasks/router/task.js
@@ -18,13 +18,65 @@ router.get('/list' ,isAutenticated,httpHandler(async (req, res) => {
     res.send(collection)
 }))
 
-router.get('/taskId/:id', httpHandler( async (req, res) => {
+router.get('/taskId/:id', isAutenticated,httpHandler( async (req, res) => {
     if (!req.params.id){
         return res.status(400).end('Missing parameter id')
     }
+    // console.log(req.params.id)
     let item = await taskService.getTaskById(req.params.id);
     if(!item) {
         return res.status(404).end(`Item with id "${req.params.id}" not found`) }
-        return item
+
+        // console.log(item,'item')
+    res.status(200).send(item)
 }))
+
+router.delete('/delete/:id', isAutenticated, httpHandler(async (req, res) => {
+       const  deleted =  await taskService.removeTask({_id:req.params.id})
+       if (!deleted) {
+           return res.status(404).end(`No such task exists with id ${req.params.id}`)
+       }
+       res.status(200).send(`task has been deleted`)
+}))
+
+// Define route to search tasks by title
+router.get('/search', httpHandler(async (req, res) => {
+   const title = req.body.title
+    if (!title) {
+        return res.status(400).json({ error: 'Title parameter is required for search' });
+      }
+    
+      const results = await taskService.searchTasks(title);
+       
+      res.send(results);
+      
+    //   const collect = await  taskService.searchTasks(req.query.title)
+    //   res.json(collect)
+  }));
+
+  router.get('/filter', httpHandler(async(req,res)=>{
+
+    const { status, priority } = req.query; // Get status and priority query parameters from request
+
+    // Validate status and priority parameters
+    if (!status && !priority) {
+      return res.status(400).json({ error: 'At least one of status or priority parameters is required' });
+    }
+    
+
+    const filteredItems= await taskService.filterByField(req.query)
+       res.send(filteredItems)
+
+    }))
+
+
+    router.get('/pagination',isAutenticated,httpHandler(async(req,res)=>{
+        const { page = 1, limit = 10 } = req.query; // Get page and limit query parameters from request
+        const query = {page,limit}
+         // Convert page and limit to number
+       const collect = await taskService.countDocs(query)
+       res.send(collect)
+
+    }))
+  
 export default router
--- server/src/modules/tasks/services/task.js
+++ server/src/modules/tasks/services/task.js
@@ -22,14 +22,82 @@ const getTaskList = async()=>{
      return  collect;
 }
 
-const getTaskById = (id)=>{
-     return id
+const getTaskById = async(id)=>{
+
+    const result = await  taskModel.findOne({ _id: id })
+    if(!result) throw createError(StatusCodes.NOT_FOUND,"Not Found")    
+    else return result;
+    // console.log(id,'idd')
+    //  return id
+}
+
+
+const removeTask = async({_id})=>{
+       const result = await  taskModel.findByIdAndRemove({_id});
+       return result;
+}
+
+const searchTasks = async(title)=>{
+    try {
+        // const { title } = req.query; // Get title query parameter from request
+    
+        // Validate title parameter
+        
+    
+        // Query tasks collection based on title (using a case-insensitive regex search)
+        const tasks = await taskModel.find({ title: { $regex: new RegExp(title, 'i') } });
+        return tasks
+        // Return search results as response
+        // res.json(tasks);
+      } catch (error) {
+        console.error('Error searching tasks:', error);
+        throw createError(StatusCodes.NOT_FOUND,"No task Found") 
+        // res.status(500).json({ error: 'Internal server error' });
+      }
+}
+
+const filterByField = async(query)=>{
+    const { status, priority } = query;
+    const filter = {};
+    if (status) filter.status = status;
+    if (priority) filter.priority = priority;
+     
+    const tasks = await taskModel.find(filter);
+    console.log(tasks,'tasks')
+    return tasks;
+    // const tasks = await taskModel.find(filter);
+    // return tasks
+}
+
+
+const countDocs = async(query)=>{
+    const { page = 1, limit = 10 } = query;
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+
+       const skip = (pageNumber - 1) * limitNumber;
+       const tasks = await taskModel.find({})
+       .skip(skip)
+       .limit(limitNumber);
+
+       const totalTasks = await taskModel.countDocuments();
+        // console.log(totalTasks,'total')
+    return {
+        tasks,
+        totalPages: Math.ceil(totalTasks / limitNumber),
+        currentPage: pageNumber
+  
+    }
 }
 
 const taskService = {
     addTask,
     getTaskList ,
-    getTaskById
+    getTaskById,
+    removeTask,
+    searchTasks,
+    filterByField ,
+    countDocs
     // login,
 }
 
